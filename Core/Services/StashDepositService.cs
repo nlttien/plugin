e421a254097f98d32196ec5e994f543392bc3630
ShopAutoBuyer Developer <dev@shopautobuyer.local>
2026-08-18 14:01:20 +0700

    fix: add using ExileCore.PoEMemory in StashDepositService

--- Core/Services/StashDepositService.cs
+++ Core/Services/StashDepositService.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Threading;
 using System.Windows.Forms;
 using ExileCore;
+using ExileCore.PoEMemory;
 using ExileCore.PoEMemory.Elements;
 using ExileCore.PoEMemory.Elements.InventoryElements;
 using ExileCore.Shared;
